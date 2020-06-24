afb971fc827a26d79fed5523229a354b941d5da6
NikolayTsikunov <nikolay.tsikunov@mail.ru>
2020-06-24 17:26:16 +0300

    Add entity

---
+++ src/main/java/by/tsikunov/day4_1/entity/NumberArray.java
@@ -0,0 +1,80 @@
+package by.tsikunov.day4_1.entity;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class NumberArray {
+
+    private static final int DEFAULT_SIZE = 10;
+    private int[] array;
+
+    public NumberArray() {
+        this.array = new int[DEFAULT_SIZE];
+    }
+
+    public NumberArray(int size) {
+        if(size > 0) {
+            this.array = new int[size];
+        } else {
+            this.array = new int[DEFAULT_SIZE];
+        }
+    }
+
+    public NumberArray(int[] array) {
+        if(array != null && array.length > 0) {
+            this.array = array;
+        } else {
+            this.array = new int[DEFAULT_SIZE];
+        }
+    }
+
+    public boolean fillArray(int[] array) {
+        boolean result = false;
+        if(array != null && array.length > 0) {
+            this.array = array;
+            result = true;
+        }
+        return result;
+    }
+
+    public OptionalInt get(int index) {
+        if(index >= 0 && index < array.length) {
+            return OptionalInt.of(array[index]);
+        } else {
+            return OptionalInt.empty();
+        }
+    }
+
+    public boolean set(int index, int number) {
+        if(index < 0 || index > array.length - 1) {
+            return false;
+        }
+        array[index] = number;
+        return true;
+    }
+
+    @Override // TODO: 24.06.2020 Arrays.toString??? 
+    public String toString() {
+        StringBuilder sb = new StringBuilder("NumberArray{");
+        sb.append("array=").append(Arrays.toString(array));
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        NumberArray entryObj = (NumberArray) obj;
+        return Arrays.equals(this.array, entryObj.array);
+    }
+
+    @Override // TODO: 24.06.2020 remake hashCode Arrays???
+    public int hashCode() {
+        return Arrays.hashCode(array);
+    }
+}
